595bfe477510bf0013112106a0193f33d407fab2
4eJIoBeG <pavelshashenko@gmail.com>
2024-09-01 22:29:57 +0300

    [add] components: product-card, product-card-group [add] dependencies: zustand [add] store: setCategoryId

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-import { Container, Filters, Title, TopBar } from '@/components/shared'
+import {
+	Container,
+	Filters,
+	ProductCard,
+	ProductsGroupList,
+	Title,
+	TopBar,
+} from '@/components/shared'
 
 export default function Home() {
 	return (
@@ -16,7 +23,84 @@ export default function Home() {
 					</div>
 					{/* Товары */}
 					<div className='flex-1'></div>
-					<div className='flex flex-col gap-16'>Список товаров</div>
+					<div className='flex flex-col gap-16'>
+						<ProductsGroupList
+							title='Пицца'
+							categoryId={1}
+							items={[
+								{
+									id: 1,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+								{
+									id: 2,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+								{
+									id: 3,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+								{
+									id: 4,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+							]}
+						/>
+						<ProductsGroupList
+							title='Комбо'
+							categoryId={2}
+							items={[
+								{
+									id: 1,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+								{
+									id: 2,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+								{
+									id: 3,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+								{
+									id: 4,
+									name: 'Вкусная',
+									imageUrl:
+										'https://media.dodostatic.net/image/r:292x292/11EE7D60FDA22358AC33C6A44EB093A2.avif',
+									price: 550,
+									items: [{ price: 550 }, { price: 650 }, { price: 700 }],
+								},
+							]}
+						/>
+					</div>
 				</div>
 			</Container>
 		</>
